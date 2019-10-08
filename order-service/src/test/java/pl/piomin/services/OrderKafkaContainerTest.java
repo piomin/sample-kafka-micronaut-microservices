c008dae2b218d47a584dd8e1b96cbdfab0bec7cb
minkowp <piotr.minkowski@play.pl>
2019-10-08 12:24:14 +0200

    test fixes

--- order-service/src/test/java/pl/piomin/services/OrderKafkaContainerTest.java
+++ order-service/src/test/java/pl/piomin/services/OrderKafkaContainerTest.java
@@ -16,10 +16,12 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.piomin.services.client.OrderClient;
 import pl.piomin.services.model.Order;
+import pl.piomin.services.model.OrderStatus;
 import pl.piomin.services.model.OrderType;
 import pl.piomin.services.repository.OrderInMemoryRepository;
 
 import javax.inject.Inject;
+import java.util.Optional;
 
 @MicronautTest
 @Testcontainers
@@ -59,6 +61,23 @@ public class OrderKafkaContainerTest {
 
     @Test
     @org.junit.jupiter.api.Order(1)
+    public void testWaiting() throws InterruptedException {
+        Order order = new Order(OrderType.NEW_TRIP, 1L, 50, 30);
+        order = repository.add(order);
+        client.send(order);
+        Order orderSent = null;
+        for (int i = 0; i < 10; i++) {
+            orderSent = orderHolder.getCurrentOrder();
+            if (orderSent != null)
+                break;
+            Thread.sleep(1000);
+        }
+        orderHolder.setCurrentOrder(null);
+        Assertions.assertNull(orderSent);
+    }
+
+    @Test
+    @org.junit.jupiter.api.Order(2)
     public void testAddNewTripOrder() throws InterruptedException {
         Order order = new Order(OrderType.NEW_TRIP, 1L, 50, 30);
         order = repository.add(order);
@@ -73,6 +92,24 @@ public class OrderKafkaContainerTest {
         orderHolder.setCurrentOrder(null);
         Assertions.assertNotNull(orderSent);
         Assertions.assertEquals(order.getId(), orderSent.getId());
+    }
+
+    @Test
+    @org.junit.jupiter.api.Order(3)
+    public void testCancelTripOrder() throws InterruptedException {
+        Order order = new Order(OrderType.CANCEL_TRIP, 1L, 50, 30);
+        client.send(order);
+        Order orderReceived = null;
+        for (int i = 0; i < 10; i++) {
+            orderReceived = orderHolder.getCurrentOrder();
+            if (orderReceived != null)
+                break;
+            Thread.sleep(1000);
+        }
+        orderHolder.setCurrentOrder(null);
+        Optional<Order> oo = repository.findById(1L);
+        Assertions.assertTrue(oo.isPresent());
+        Assertions.assertEquals(OrderStatus.REJECTED, oo.get().getStatus());
     }
 
 }
--- order-service/src/test/java/pl/piomin/services/OrderKafkaEmbeddedTest.java
+++ order-service/src/test/java/pl/piomin/services/OrderKafkaEmbeddedTest.java
@@ -28,7 +28,7 @@ import javax.inject.Inject;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class OrderKafkaEmbeddedTest {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(OrderKafkaEmbeddedTest.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderKafkaEmbeddedTest.class);
 
     @Inject
     OrderClient client;
@@ -41,15 +41,45 @@ public class OrderKafkaEmbeddedTest {
 
     @BeforeAll
     public void init() {
-		LOGGER.info("Topics: {}", kafkaEmbedded.getKafkaServer().get().zkClient().getAllTopicsInCluster());
-	}
+        LOGGER.info("Topics: {}", kafkaEmbedded.getKafkaServer().get().zkClient().getAllTopicsInCluster());
+    }
 
     @Test
-	@org.junit.jupiter.api.Order(1)
+    @org.junit.jupiter.api.Order(1)
     public void testAddNewTripOrder() throws InterruptedException {
         Order order = new Order(OrderType.NEW_TRIP, 1L, 50, 30);
         order = repository.add(order);
         client.send(order);
+        Order orderSent = waitForOrder();
+        Assertions.assertNotNull(orderSent);
+        Assertions.assertEquals(order.getId(), orderSent.getId());
+    }
+
+    @Test
+    @org.junit.jupiter.api.Order(2)
+    public void testCancelTripOrder() throws InterruptedException {
+        Order order = new Order(OrderType.CANCEL_TRIP, 1L, 50, 30);
+        client.send(order);
+        Order orderReceived = waitForOrder();
+        Optional<Order> oo = repository.findById(1L);
+        Assertions.assertTrue(oo.isPresent());
+        Assertions.assertEquals(OrderStatus.REJECTED, oo.get().getStatus());
+    }
+
+    @Test
+    @org.junit.jupiter.api.Order(3)
+    public void testPaymentTripOrder() throws InterruptedException {
+        Order order = new Order(OrderType.PAYMENT_PROCESSED, 1L, 50, 30);
+        order.setTripId(1L);
+        order = repository.add(order);
+        client.send(order);
+        Order orderSent = waitForOrder();
+        Optional<Order> oo = repository.findById(order.getId());
+        Assertions.assertTrue(oo.isPresent());
+        Assertions.assertEquals(OrderStatus.COMPLETED, oo.get().getStatus());
+    }
+
+    private Order waitForOrder() throws InterruptedException {
         Order orderSent = null;
         for (int i = 0; i < 10; i++) {
             orderSent = orderHolder.getCurrentOrder();
@@ -58,45 +88,7 @@ public class OrderKafkaEmbeddedTest {
             Thread.sleep(1000);
         }
         orderHolder.setCurrentOrder(null);
-        Assertions.assertNotNull(orderSent);
-        Assertions.assertEquals(order.getId(), orderSent.getId());
+        return orderSent;
     }
 
-	@Test
-	@org.junit.jupiter.api.Order(2)
-	public void testCancelTripOrder() throws InterruptedException {
-		Order order = new Order(OrderType.CANCEL_TRIP, 1L, 50, 30);
-		client.send(order);
-		Order orderReceived = null;
-		for (int i = 0; i < 10; i++) {
-			orderReceived = orderHolder.getCurrentOrder();
-			if (orderReceived != null)
-				break;
-			Thread.sleep(1000);
-		}
-		orderHolder.setCurrentOrder(null);
-		Optional<Order> oo = repository.findById(1L);
-		Assertions.assertTrue(oo.isPresent());
-		Assertions.assertEquals(OrderStatus.REJECTED, oo.get().getStatus());
-	}
-
-	@Test
-	@org.junit.jupiter.api.Order(3)
-	public void testPaymentTripOrder() throws InterruptedException {
-		Order order = new Order(OrderType.PAYMENT_PROCESSED, 1L, 50, 30);
-		order.setTripId(1L);
-		order = repository.add(order);
-		client.send(order);
-		Order orderSent = null;
-		for (int i = 0; i < 10; i++) {
-			orderSent = orderHolder.getCurrentOrder();
-			if (orderSent != null)
-				break;
-			Thread.sleep(1000);
-		}
-		orderHolder.setCurrentOrder(null);
-		Optional<Order> oo = repository.findById(order.getId());
-		Assertions.assertTrue(oo.isPresent());
-		Assertions.assertEquals(OrderStatus.COMPLETED, oo.get().getStatus());
-	}
 }
